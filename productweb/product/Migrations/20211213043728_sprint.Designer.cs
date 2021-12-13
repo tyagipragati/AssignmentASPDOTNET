f9b0e266e317ba05fecec133fc1e5bd5205d52b5
Pragati Tyagi <Pragati.Tyagi@securemeters.com>
2021-12-13 10:11:11 +0530

    Deleted Redundant files

--- productweb/product/Migrations/20211213043728_sprint.Designer.cs
+++ productweb/product/Migrations/20211213043728_sprint.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using product.Data;
 
@@ -10,9 +11,10 @@ using product.Data;
 namespace product.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211213043728_sprint")]
+    partial class sprint
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ productweb/product/Migrations/20211213043728_sprint.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace product.Migrations
+{
+    public partial class sprint : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Gasmeter",
+                table: "Gasmeter");
+
+            migrationBuilder.RenameTable(
+                name: "Gasmeter",
+                newName: "liberty");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_liberty",
+                table: "liberty",
+                column: "SerialNumber");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_liberty",
+                table: "liberty");
+
+            migrationBuilder.RenameTable(
+                name: "liberty",
+                newName: "Gasmeter");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Gasmeter",
+                table: "Gasmeter",
+                column: "SerialNumber");
+        }
+    }
+}
